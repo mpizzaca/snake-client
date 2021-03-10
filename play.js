7440fdd9e3cdd937e3f649a059ea37e234bc523b
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-10 12:40:22 -0500

    connect with name!

--- play.js
+++ play.js
@@ -3,6 +3,10 @@ const  { connect } = require('./client');
 console.log('Connecting ...');
 const conn = connect();
 
+conn.on('connect', () => {
+  conn.write('Name: MP!');
+});
+
 conn.on('data', data => {
   console.log(data);
 });
